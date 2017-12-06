6d7862f31d6fa8976ed4a5e427549546cfcbc561
Ankita Jangra <ankita.jangra@quovantis.com>
2017-12-06 19:12:27 +0530

    Test waitForElementMethod

---
+++ testCases/SelectCampaignPageTest.js
@@ -0,0 +1,14 @@
+var selectCampPage = require('../pages/SelectCampaignPage.js')
+
+
+describe('Verify the Campaign Selection which needs action to be performed', function () {
+
+
+    //Positive flow
+    it('Search for the expected campaign', function () {
+
+        selectCampPage.searchCampaign()
+        })
+
+
+    })
