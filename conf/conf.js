2ff5a7b54d2dc040172f847018435d5ff03f5c4f
Vibhor Mathur <vibhor.mathur@quovantis.com>
2019-05-16 12:40:07 +0530

    spec path

--- conf/conf.js
+++ conf/conf.js
@@ -1,7 +1,6 @@
 // let dirname = process.env['FOLDER_PATH'];
 
-//let dirname = '/home/ubuntu/';
-let dirname= '/var/lib/jenkins/workspace/Protractortest';
+let dirname = 'Protractortest';
 let log4js = require('log4js');
 let Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
 // let nodeMailer = require('nodemailer');
